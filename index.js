25ed790bc882643adcc9a411cac566acf1479d26
Rohan Grover <grohan95@gmail.com>
2019-08-27 01:18:21 +0530

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+ 
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8080 })
+var res='',count='';//results
+var fs = require('fs');
+process.argv[2]?count=Number(process.argv[2]):count=10;
+
+wss.on('connection', ws => {
+fs.watchFile('test.txt',{interval:500}, (curr, prev) => {
+let n = count;
+res=[];
+var testData = fs.readFileSync('test.txt', 'utf8').split('\n');
+while(n!=0 && testData.length>0){
+res.push(testData.pop());
+n--;
+}
+ws.send(JSON.stringify(res));
+});
+});
---
+++ js/data.js
@@ -0,0 +1,30 @@
+let socket = new WebSocket("ws://localhost:8080");
+
+socket.onopen = function(e) {
+  console.log("Connection established");
+};
+
+socket.onmessage = function(event) {
+  var flData = JSON.parse(event.data);
+  console.log(flData);
+  document.getElementById("result").innerHTML = "-----Tail-test.txt-------";
+  flData.map((val)=>{
+    var para = document.createElement("p");
+    var node = document.createTextNode(""+val);
+    para.appendChild(node);
+    var element = document.getElementById("result");
+    element.appendChild(para);
+  })
+};
+
+socket.onclose = function(event) {
+  if (event.wasClean) {
+    console.log(`Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+  } else {
+    console.log('Connection died');
+  }
+};
+
+socket.onerror = function(error) {
+  console.log(`[error] ${error.message}`);
+};
