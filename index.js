35428e408c1a8befd9eb3be2fe6e60574bd3cf81
ronork <grohan95@gmail.com>
2019-08-28 14:27:04 +0530

    added firstfold logic

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-
+ 
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 })
 var res='',count='';//results
 var fs = require('fs');
 process.argv[2]?count=Number(process.argv[2]):count=10;
-
-wss.on('connection', ws => {
-fs.watchFile('test.txt',{interval:500}, (curr, prev) => {
-let n = count;
-res=[];
+const getFileData = ()=>{
+let n = count,res=[];
 var testData = fs.readFileSync('test.txt', 'utf8').split('\n');
-while(n>=0 && testData.length>0){
+while(n!=0 && testData.length>0){
 res.push(testData.pop());
 n--;
 }
+return res;
+}
+wss.on('connection', ws => {
+let res = [];
+res = getFileData();
+ws.send(JSON.stringify(res));
+fs.watchFile('test.txt',{interval:500}, (curr, prev) => {
+res = getFileData();
 ws.send(JSON.stringify(res));
 });
 });
